becd5e9f969d23d2f1059e0b3282e4a383d896bc
Rachit <mailracit@gmail.com>
2025-03-17 22:52:32 +0530

    mobile view

---
+++ assets/features.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function showComparison(type) {
+        const talentBtn = document.querySelector(".tv-benefits");
+        const otherBtn = document.querySelector(".other-benefits");
+        const talentContent = document.querySelectorAll(".talent");
+        const otherContent = document.querySelectorAll(".other");
+
+        if (type === "talent") {
+            talentBtn.classList.add("active");
+            otherBtn.classList.remove("active");
+            otherContent.forEach(item => {
+                item.style.display = 'none'
+            })
+            talentContent.forEach(item => {
+                item.style.display = 'flex'
+            })
+        } else {
+            talentBtn.classList.remove("active");
+            otherBtn.classList.add("active");
+            talentContent.forEach(item => {
+                item.style.display = 'none'
+            })
+            otherContent.forEach(item => {
+                item.style.display = 'flex'
+            })
+        }
+    }
+
+    // Attach event listeners to buttons
+    document.querySelector(".tv-benefits").addEventListener("click", () => showComparison("talent"));
+    document.querySelector(".other-benefits").addEventListener("click", () => showComparison("other"));
+});
